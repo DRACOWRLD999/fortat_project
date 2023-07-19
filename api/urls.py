69a06baee717d1963b123686d14fefb3f8806e02
Ahmed Sabah <hmoodhmood004@gmail.com>
2023-07-20 00:17:44 +0300

    added search in description if there is nothing in location and destination

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import RouteList, RouteDetail, search_locations
 
+from .views import RouteDetail, RouteList, search_locations
 
 urlpatterns = [
-    path('Routes/', RouteList.as_view(), name='route-list'),
-    path('Routes/<int:pk>', RouteDetail.as_view(),name= 'route-detail'),
-    path('search/locations/',search_locations, name='search-locations'),
+    path('routes/search/',search_locations, name='search-locations'),
+    path('routes/', RouteList.as_view(), name='route-list'),
+    path('routes/<int:pk>', RouteDetail.as_view(),name= 'route-detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,10 +1,11 @@
-from rest_framework import generics
-from django.http import JsonResponse
 from django.db.models import Q
+from django.http import JsonResponse
+from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
+
 from .models import Route
 from .serializers import RouteSerializer
 
-from rest_framework.pagination import PageNumberPagination
 
 class CustomPagination(PageNumberPagination):
     page_size = 10
@@ -21,19 +22,26 @@ class RouteDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Route.objects.all()
     
 
+from django.db.models import Q
+
 
 def search_locations(request):
     if 'query' in request.GET:
         query = request.GET['query']
 
-        #case-insensitive search for similar matches
-        results = Route.objects.filter(Q(location__icontains=query) | Q(destination__icontains=query))[:10]
+        #search for similar matches in location or destination
+        location_results = Route.objects.filter(location__icontains=query)[:10]
+        destination_results = Route.objects.filter(destination__icontains=query)[:10]
 
-        # Serialize the results to JSON
-        serialized_results = [{'id': route.id, 'location': route.location, 'destination': route.destination, 'description': route.description, 'ride_fee':route.ride_fee} for route in results]
+        # If there are no results in both location and destination then search in description
+        if not location_results and not destination_results:
+            description_results = Route.objects.filter(description__icontains=query)[:10]
+            results = description_results
+        else:
+            results = location_results | destination_results
+
+        serialized_results = [{'id': route.id, 'location': route.location, 'destination': route.destination, 'description': route.description, 'ride_fee': route.ride_fee} for route in results]
         return JsonResponse(serialized_results, safe=False)
     return JsonResponse([], safe=False)
 
 
-    
-    
