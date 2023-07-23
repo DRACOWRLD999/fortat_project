d8405fdf2068cf7091a6efa08d730b1a40175315
Ahmed Sabah <hmoodhmood004@gmail.com>
2023-07-23 18:03:36 +0300

    added allowed origins

--- fortat_api/settings.py
+++ fortat_api/settings.py
@@ -134,4 +134,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+   "https://fortat.up.railway.app"
+]
