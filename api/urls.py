32ca5a6fb8fcaa3e42d97fe9e278264c21e7ca5f
Ahmed Sabah <hmoodhmood004@gmail.com>
2023-07-23 17:04:03 +0300

    added landing page

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
+from django.views.generic import TemplateView
 from .views import RouteDetail, RouteList, search_locations
 
 urlpatterns = [
     path('routes/search/',search_locations, name='search-locations'),
     path('routes/', RouteList.as_view(), name='route-list'),
     path('routes/<int:pk>', RouteDetail.as_view(),name= 'route-detail'),
+    path('', TemplateView.as_view(template_name= 'landing-page.html'))
 ]
